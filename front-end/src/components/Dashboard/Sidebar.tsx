3937da9f885aee72f73acb4b39bf2ab8b5b24401
Luis Scandelai <saeslhs@gmail.com>
2022-07-12 16:43:23 -0300

    Added sidebar title and logo on middle, finished dashboard home page

--- front-end/src/components/Dashboard/Sidebar.tsx
+++ front-end/src/components/Dashboard/Sidebar.tsx
@@ -5,11 +5,14 @@ import SideButton from "./SideButton";
 const Sidebar = () => {
   return (
     <div className={styles.sidebar}>
-      <SideButton text={"Home"} active={true} />
-      <SideButton text={"Product"} />
-      <SideButton text={"Clients"} />
-      <SideButton text={"Employees"} />
-      <SideButton text={"Sales"} />
+      <div className={`${styles.title} mt-3`}>Generic Dashboard</div>
+      <div className={`${styles.sideButtons} mt-4`}>
+        <SideButton text={"Home"} active={true} />
+        <SideButton text={"Product"} />
+        <SideButton text={"Clients"} />
+        <SideButton text={"Employees"} />
+        <SideButton text={"Sales"} />
+      </div>
     </div>
   );
 };
--- front-end/src/views/DashboardMain.tsx
+++ front-end/src/views/DashboardMain.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "./DashboardMain.module.css";
 import Sidebar from "../components/Dashboard/Sidebar";
+import logo from "../assets/logo.png";
 
 const DashboardMain = () => {
   return (
     <div className={styles.main}>
       <Sidebar />
-      <div className={styles.board}></div>
+      <div className={styles.board}>
+        <img src={logo} alt="Dashboard Logo" />
+      </div>
     </div>
   );
 };
