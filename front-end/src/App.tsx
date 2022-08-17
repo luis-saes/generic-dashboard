9c66785e2c654bc1ef7997f2bf8327e997160d8a
haeunFassto <haeun.park@fssuniverse.com>
2022-08-17 13:36:53 +0900

    redirect to login from default path

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LoginScreen from "./views/LoginScreen";
 import DashboardMain from "./views/DashboardMain";
 import ProductsScreen from "./views/ProductsScreen";
@@ -12,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />}/>
         <Route path="/login" element={<LoginScreen />} />
         <Route path="/dashboard" element={<DashboardMain />} />
         <Route path="/products" element={<ProductsScreen />} />
