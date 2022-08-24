7e8f8cf62637f2cc845fbc09beb781ea9135f331
Luis Scandelai <saeslhs@gmail.com>
2022-08-23 23:05:16 -0300

    Creating generic functions for table rows CRUD

---
+++ front-end/src/utils/Utils.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const editLine = (
+  index: number,
+  newLine: (string | number)[],
+  setTableData: React.Dispatch<React.SetStateAction<(string | number)[][]>>,
+  tableData: (string | number)[][]
+) => {
+  let newLineWithIndex: (string | number)[] = [];
+  newLineWithIndex = [...tableData[index].slice(0, 1), ...newLine];
+
+  setTableData([
+    ...tableData.slice(0, index),
+    newLineWithIndex,
+    ...tableData.slice(index + 1),
+  ]);
+};
+
+const deleteLine = (
+  index: number,
+  setTableData: React.Dispatch<React.SetStateAction<(string | number)[][]>>
+) => {
+  setTableData((currentTableData: (string | number)[][]) =>
+    currentTableData.filter((el: any) => {
+      return el[0] !== index;
+    })
+  );
+};
+
+export { editLine, deleteLine };
